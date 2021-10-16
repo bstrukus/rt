8998ea7b74f49cbc12cb60a8443179f73cf4f009
Ben Strukus <lokonity@gmail.com>
2021-10-15 17:16:28 -0700

    Renames Constants -> Utils, adds InRange function for floats & ints

---
+++ src/main/Math/Utils.cs
@@ -0,0 +1,21 @@
+/*
+ * #copyright_placeholder Copyright Ben Strukus
+ */
+
+namespace rt.Math
+{
+    public class Utils
+    {
+        public const float Epsilon = 0.000001f;
+
+        static public bool InRange(float val, float min, float max)
+        {
+            return min <= val && val <= max;
+        }
+
+        static public bool InRange(int val, int min, int max)
+        {
+            return min <= val && val <= max;
+        }
+    }
+}
