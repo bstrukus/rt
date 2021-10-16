9c9721cc60da7b9b0c965152e5052963d7b20f5d
Ben Strukus <lokonity@gmail.com>
2021-10-15 17:17:28 -0700

    Cleans up main file

--- src/main/Program.cs
+++ src/main/Program.cs
@@ -2,86 +2,11 @@
  * Copyright Ben Strukus
  */
 
-using main;
-using Newtonsoft.Json;
 using System;
 using System.IO;
 
 namespace rt
 {
-    /*
-    public class Vec3
-    {
-        public Vec3(float x, float y, float z)
-        {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        public float x { get; }
-        public float y { get; }
-        public float z { get; }
-    }
-
-    public class Image
-    {
-        [JsonProperty("stepFactor")]
-        public int StepFactor { get; set; }
-
-        [JsonProperty("fileExtension")]
-        public string FileExtension { get; set; }
-    }
-
-    public class Orientation
-    {
-        [JsonProperty("x-axis")]
-        public Vec3 xAxis { get; set; }
-
-        [JsonProperty("y-axis")]
-        public Vec3 yAxis { get; set; }
-
-        [JsonProperty("z-axis")]
-        public Vec3 zAxis { get; set; }
-    }
-
-    public class Transform
-    {
-        [JsonProperty("position")]
-        public Vec3 position { get; set; }
-
-        [JsonProperty("orientation")]
-        public Orientation orientation { get; set; }
-
-        [JsonProperty("scale")]
-        public Vec3 scale { get; set; }
-    }
-
-    public class Camera
-    {
-        [JsonProperty("transform")]
-        public Transform transform { get; set; }
-    }
-
-    //     public IShape
-    //         {
-    public class Scene
-    {
-        // Image
-        [JsonProperty("image")]
-        public Image Image { get; set; }
-
-        // Camera
-        //[JsonProperty("camera")]
-
-        // Shapes
-        [JsonProperty("shapes")]
-        public Shape[] shapes { get; set; }
-
-        // Lights
-    }
-    */
-
     internal class Program
     {
         private const string ScenePath = "..\\..\\scenes\\";
