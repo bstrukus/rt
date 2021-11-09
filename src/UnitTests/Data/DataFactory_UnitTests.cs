4f6cea7600a53276072ac41e7c069bdfdfe52769
Ben Strukus <lokonity@gmail.com>
2021-11-09 08:11:17 -0800

    Stubs out unit tests for DataFactory

---
+++ src/UnitTests/Data/DataFactory_UnitTests.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * #copyright_placeholder Copyright Ben Strukus
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Data
+{
+    [TestClass]
+    public class DataFactory_UnitTests
+    {
+        [TestMethod]
+        public void LoadInValidSceneWithNoIssues()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void LoadInSceneWithMissingDataAndRegisterIt()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(false);
+        }
+
+        [TestMethod]
+        public void LoadInSpheresAndBoxes()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(false);
+        }
+    }
+}
