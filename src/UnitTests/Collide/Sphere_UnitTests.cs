7512d98e19567e38a5cfc88c482b3d869ff2ab0d
Ben Strukus <lokonity@gmail.com>
2021-11-11 07:29:45 -0800

    Initialized new file for sphere unit testing

---
+++ src/UnitTests/Collide/Sphere_UnitTests.cs
@@ -0,0 +1,13 @@
+﻿/*
+ * #copyright_placeholder Copyright Ben Strukus
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Collide
+{
+    [TestClass]
+    public class Sphere_UnitTests
+    {
+    }
+}
