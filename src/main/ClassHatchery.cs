39b58e7bdf6d55b3054b1a31d5232bb581d32740
Ben Strukus <lokonity@gmail.com>
2021-11-12 06:22:05 -0800

    Moves the Log class to its own file

--- src/main/ClassHatchery.cs
+++ src/main/ClassHatchery.cs
@@ -387,27 +387,4 @@ namespace rt
             }
         }
     }
-
-    namespace Utility
-    {
-        using System;
-
-        internal static class Log
-        {
-            static public void Info(string info)
-            {
-                Console.WriteLine($"INFO: {info}");
-            }
-
-            static public void Warning(string warning)
-            {
-                Console.WriteLine($"WARNING: {warning}");
-            }
-
-            static public void Error(string error)
-            {
-                Console.WriteLine($"ERROR: {error}");
-            }
-        }
-    }
 }
---
+++ src/main/Utility/Log.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * #copyright_placeholder Copyright Ben Strukus
+ */
+
+namespace Utility
+{
+    using System;
+
+    internal static class Log
+    {
+        static public void Info(string info)
+        {
+            Console.WriteLine($"INFO: {info}");
+        }
+
+        static public void Warning(string warning)
+        {
+            Console.WriteLine($"WARNING: {warning}");
+        }
+
+        static public void Error(string error)
+        {
+            Console.WriteLine($"ERROR: {error}");
+        }
+    }
+}
