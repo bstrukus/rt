a257ddd801f58de5aab67628763f452d197f89b2
Ben Strukus <lokonity@gmail.com>
2021-11-18 10:24:40 -0800

    Builds boxes, reorganizes some helper functions

--- src/main/Data/DataFactory.cs
+++ src/main/Data/DataFactory.cs
@@ -45,13 +45,13 @@ namespace rt.Data
 
             // Go through the list of Spheres & Boxes to create a list of IHittables
             List<IHittable> hittables = new List<IHittable>();
-            if (shapeCollection.HasSpheres())
+            if (shapeCollection.HasSpheres() && false)
             {
                 foreach (var sphereData in shapeCollection.Spheres)
                 {
                     hittables.Add(new Sphere(
-                        this.CreateTransform(sphereData.Transform),
-                        this.CreateMaterial(sphereData.Material),
+                        CreateTransform(sphereData.Transform),
+                        CreateMaterial(sphereData.Material),
                         (float)sphereData.Radius
                         ));
                 }
@@ -61,10 +61,23 @@ namespace rt.Data
             {
                 foreach (var boxData in shapeCollection.Boxes)
                 {
-                    hittables.Add(new Box(
-                        this.CreateTransform(boxData.Transform),
-                        this.CreateMaterial(boxData.Material)
-                        ));
+                    if (boxData.HasTransform())
+                    {
+                        hittables.Add(new Box(
+                            CreateTransform(boxData.Transform),
+                            CreateMaterial(boxData.Material)
+                            ));
+                    }
+                    else if (boxData.HasCornerAndDirectionVectors())
+                    {
+                        hittables.Add(new Box(
+                            CreateVec3(boxData.Corner),
+                            CreateVec3(boxData.LengthVector),
+                            CreateVec3(boxData.WidthVector),
+                            CreateVec3(boxData.HeightVector),
+                            CreateMaterial(boxData.Material)
+                            ));
+                    }
                 }
             }
 
@@ -76,8 +89,8 @@ namespace rt.Data
                 foreach (var pointLightData in lightCollection.PointLights)
                 {
                     lights.Add(new PointLight(
-                        this.CreateTransform(pointLightData.Transform),
-                        DoubleListToVec3(pointLightData.Color)
+                        CreateTransform(pointLightData.Transform),
+                        CreateVec3(pointLightData.Color)
                         ));
                 }
             }
@@ -90,7 +103,7 @@ namespace rt.Data
             var cameraData = this.sceneData.Camera;
 
             return new Camera(
-                eyeDirection: DoubleListToVec3(cameraData.EyeDirection),
+                eyeDirection: CreateVec3(cameraData.EyeDirection),
                 plane: this.CreateProjectionPlane());
         }
 
@@ -109,58 +122,61 @@ namespace rt.Data
             return new Image(width, height, imageData.FileName);
         }
 
-        private ProjectionPlane CreateProjectionPlane()
-        {
-            var projectionPlaneData = this.sceneData.Camera.ProjectionPlane;
-
-            return new ProjectionPlane(
-                center: DoubleListToVec3(projectionPlaneData.Center),
-                horizontalAxis: DoubleListToVec3(projectionPlaneData.UAxis),
-                verticalAxis: DoubleListToVec3(projectionPlaneData.VAxis));
-        }
-
-        private Transform CreateTransform(TransformData data)
-        {
-            return new Transform(
-                position: DoubleListToVec3(data.Position),
-                orientation: DoubleListToQuat(data.Orientation),
-                scale: DoubleListToVec3(data.Scale));
-        }
-
-        private Material CreateMaterial(MaterialData data)
-        {
-            return new Material(
-                color: DoubleListToVec3(data.Diffuse));
-        }
-
-        public static Math.Vec3 DoubleListToVec3(List<double> vec3)
-        {
-            Debug.Assert(vec3 != null);
-            Debug.Assert(vec3.Count == 3);
-            return new Math.Vec3((float)vec3[0], (float)vec3[1], (float)vec3[2]);
-        }
-
-        public static Math.Quat DoubleListToQuat(List<double> quat)
-        {
-            Debug.Assert(quat != null);
-            Debug.Assert(quat.Count == 4);
-            return new Math.Quat((float)quat[0], (float)quat[1], (float)quat[2], (float)quat[3]);
-        }
-
-        public static bool ValidateList(List<double> vector, int expectedCount)
+        public static bool ValidateList(List<double> vector, int expectedCount, bool shouldBeNormalizedValues = true)
         {
             if (vector != null && vector.Count == expectedCount)
             {
-                foreach (var value in vector)
+                if (shouldBeNormalizedValues)
                 {
-                    if (!Numbers.InRange(value, 0.0f, 1.0f))
+                    foreach (var value in vector)
                     {
-                        return false;
+                        if (!Numbers.InRange(value, 0.0f, 1.0f))
+                        {
+                            return false;
+                        }
                     }
                 }
                 return true;
             }
             return false;
+        }
+
+        private ProjectionPlane CreateProjectionPlane()
+        {
+            var projectionPlaneData = this.sceneData.Camera.ProjectionPlane;
+
+            return new ProjectionPlane(
+                center: CreateVec3(projectionPlaneData.Center),
+                horizontalAxis: CreateVec3(projectionPlaneData.UAxis),
+                verticalAxis: CreateVec3(projectionPlaneData.VAxis));
+        }
+
+        private static Transform CreateTransform(TransformData data)
+        {
+            return new Transform(
+                position: CreateVec3(data.Position),
+                orientation: CreateQuat(data.Orientation),
+                scale: CreateVec3(data.Scale));
+        }
+
+        private static Material CreateMaterial(MaterialData data)
+        {
+            return new Material(
+                color: CreateVec3(data.Diffuse));
+        }
+
+        private static Math.Vec3 CreateVec3(List<double> vec3)
+        {
+            Debug.Assert(ValidateList(vec3, 3));
+
+            return new Math.Vec3((float)vec3[0], (float)vec3[1], (float)vec3[2]);
+        }
+
+        private static Math.Quat CreateQuat(List<double> quat)
+        {
+            Debug.Assert(ValidateList(quat, 4));
+
+            return new Math.Quat((float)quat[0], (float)quat[1], (float)quat[2], (float)quat[3]);
         }
     }
 }
