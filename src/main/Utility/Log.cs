3f054f69fa2e989d2bb42bd5607e12087e223ec4
Ben Strukus <lokonity@gmail.com>
2021-12-07 08:41:11 -0800

    Adds flags to gate the different types of logs

--- src/main/Utility/Log.cs
+++ src/main/Utility/Log.cs
@@ -2,30 +2,36 @@
  * #copyright_placeholder Copyright Ben Strukus
  */
 
+using System;
+
 namespace rt.Utility
 {
-    using System;
-
     /// <summary>
     /// Helper class to handle logging out information of varying severity.
     /// </summary>
     internal static class Log
     {
+        private const bool LogInfo = true;
+        private const bool LogWarning = true;
+        private const bool LogError = true;
         // #todo Create functions that log START when called and END at end of scope, possibly using "using" functionality
 
         public static void Info(string info)
         {
-            Console.WriteLine($"INFO: {info}");
+            if (LogInfo)
+                Console.WriteLine($"INFO: {info}");
         }
 
         public static void Warning(string warning)
         {
-            Console.WriteLine($"WARNING: {warning}");
+            if (LogWarning)
+                Console.WriteLine($"WARNING: {warning}");
         }
 
         public static void Error(string error)
         {
-            Console.WriteLine($"ERROR: {error}");
+            if (LogError)
+                Console.WriteLine($"ERROR: {error}");
         }
 
         public static void Write(string text)
